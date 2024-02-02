ef19512ef4a0c7c0bc42a0f006f2a37e1a36ae66
Pacvh <mailmail@gmail.com>
2024-02-02 15:31:50 +0300

    addded fill script

--- manage.py
+++ manage.py
@@ -28,8 +28,10 @@ elif (arguments[1] == "cleardb"):
     from Database.python_scripts.delete_all_tables import delete_all_tables
     delete_all_tables(dotenv_path)
 elif (arguments[1] == "filldb"):
-    from Database.python_scripts.fill_db_with_data import fill_db_with_data
+    from Database.python_scripts.fill_db_with_users import fill_db_with_data
+    from Database.python_scripts.fill_db_with_machines import fill_db_with_machines
     fill_db_with_data(dotenv_path)
+    fill_db_with_machines(dotenv_path)
 else:
     print("argument doesn't exist")
     print("try : manage.py help")
